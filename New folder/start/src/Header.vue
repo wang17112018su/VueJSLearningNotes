72f1609bd04e92c4352181c4f271c984190d5be8
James Wang <wangshijun@kapps.com.sg>
2018-12-19 10:34:27 +0800

    sample of organising folder

---
+++ New folder/start/src/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <header>
+          <h1>Server Status</h1>
+        </header>
+        <hr>
+      </div>
+    </div>
+    <div>
+      <app-server-report></app-server-report>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <hr>
+        <footer>
+          <p>All Servers are managed here</p>
+        </footer>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import ServerReport from './ServerReport.vue';
+
+    export default {
+      components: {
+      'app-server-report': ServerReport
+      }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ New folder/start/src/ServerReport.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+      <ul class="list-group">
+        <li
+          class="list-group-item"
+          v-for="index in 5">
+          Server #{{ index }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
